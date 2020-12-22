cba7fb4337f124c6251351b03837fff3df205cbf
AshleyThomasDevelopment <ashleythomasdevelopment@gmail.com>
2020-12-22 01:01:04 -0500

    refactoring

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/* --- --- --- --- JavaScript for Arnold's Game --- --- --- --- */
+/* JavaScript for Arnold's Game */
 
 /* LIST OF VARIABLES AND ARRAYS */
 
@@ -25,6 +25,7 @@ let arnoldBool = false;
 /* SOUNDS ARRAY AND PICKER */
 
 function soundBankPicker() {
+
   if (Math.ceil(Math.random() * 3) === 1) {
     player1Sounds = starTrekSounds;
   } else if (Math.ceil(Math.random() * 3) === 2) {
@@ -32,6 +33,7 @@ function soundBankPicker() {
   } else {
     player1Sounds = marioSounds;
   }
+
 }
 
 function forceArnold() {
@@ -40,11 +42,13 @@ function forceArnold() {
 
 function arnoldModeSwitch() {
   player1Sounds = arnoldBank[arnoldCounter];
-  if ( arnoldCounter == arnoldBank.length - 1 ) {
+
+  if (arnoldCounter == arnoldBank.length - 1) {
     arnoldCounter = 0;
   } else {
     arnoldCounter++;
   }
+
 }
 
 $("#forceArnold").on("dblclick", function () {
@@ -53,17 +57,20 @@ $("#forceArnold").on("dblclick", function () {
 });
 
 $("#forceArnold").on("click", function () {
-  if ( player1Sounds == arnoldSounds ) {
+  if (player1Sounds == arnoldSounds) {
     arnoldCounter++;
   }
+
   player1Sounds = arnoldBank[arnoldCounter];
   arnoldBool = false;
   $("#forceArnold").css("color", "var(--darkblue)");
-  if ( arnoldCounter == arnoldBank.length - 1 ) {
+
+  if (arnoldCounter == arnoldBank.length - 1) {
     arnoldCounter = 0;
   } else {
     arnoldCounter++;
   }
+
 });
 
 var arnoldSounds = [
@@ -98,7 +105,7 @@ var arnoldSounds4 = [
   "sounds/arnold4/mars.mp3",
 ]
 
-arnoldBank = [arnoldSounds, arnoldSounds2, arnoldSounds3, arnoldSounds4,];
+arnoldBank = [arnoldSounds, arnoldSounds2, arnoldSounds3, arnoldSounds4, ];
 
 var marioSounds = [
   "sounds/mario/smb_1-up.wav",
@@ -116,18 +123,20 @@ var starTrekSounds = [
   "sounds/tng/autodestructsequencearmed_ep.mp3",
 ]
 
+
 /* SQUARE ANIMATION AND SOUND */
 
 function squareActive() {
   $("#" + sqId + "-box").addClass("anim_sq-pulse");
+
   setTimeout(function () {
     $("#" + sqId + "-box").removeClass("anim_sq-pulse");
   }, 200);
+
   var sq1Sound = new Audio(player1Sounds[0]);
   var sq2Sound = new Audio(player1Sounds[1]);
   var sq3Sound = new Audio(player1Sounds[2]);
   var sq4Sound = new Audio(player1Sounds[3]);
-
 
   switch (sqId) {
     case "sq1":
@@ -146,23 +155,18 @@ function squareActive() {
       currentAudio = sq3Sound;
       sq3Sound.load();
       sq3Sound.play();
-
       break;
 
     case "sq4":
       currentAudio = sq4Sound;
       sq4Sound.load();
       sq4Sound.play();
-
       break;
 
     default:
   }
 }
 
-
-
-/* ------ GAME STAGES ------ */
 
 /* INVITE SCREEN */
 
@@ -177,22 +181,26 @@ function playButtonClickMove(playBtn) {
   playBtn.css("left", "-=2");
   playBtn.css("top", "+=3");
   playBtn.css("box-shadow", "-6px 2px 11px 3px var(--black)")
+
   setTimeout(function () {
     playBtn.css("left", "+=2");
     playBtn.css("top", "-=3");
     playBtn.css("box-shadow", "-8px 5px 11px 3px var(--black)")
   }, 60);
+
 }
 
 function invitePaneFade() {
   $(".ps-backdrop").addClass("is_invite-fade");
   $(".ps-invite-pane").addClass("is_invite-fade");
+
   setTimeout(function () {
     $(".ps-invite-pane").css("display", "none");
     $(".ps-invite-pane").removeClass("is_invite-fade");
     $(".ps-backdrop").removeClass("is_invite-fade");
     $(".ps-backdrop").css("display", "none");
   }, 2000);
+
 }
 
 function playButtonReset() {
@@ -203,23 +211,30 @@ function playButtonReset() {
     playButtonClickMove(playBtn);
     initializeGame();
   });
+
 }
+
 
 /* START GAME */
 
 function initializeGame() {
   invitePaneFade();
+
   setTimeout(function () {
     levelBox();
   }, 1700);
+
   currentLevel = 1;
+
   setTimeout(function () {
     instructions(2);
   }, 1000);
+
   setTimeout(function () {
     beginGame();
     playButtonReset();
   }, 4000);
+
 }
 
 function beginGame() {
@@ -234,15 +249,19 @@ function beginGame() {
 
 function arnoldsTurn() {
   let newNumber = Math.ceil(Math.random() * 4);
+
   if (player1Sounds == arnoldSounds && currentLevel < 6 && newNumber === 3) {
     newNumber = 1;
   }
+
   sPat.push(newNumber);
   sqId = "sq" + newNumber;
   squareActive();
+
   setTimeout(function () {
     playerTurn();
   }, 200);
+
 }
 
 
@@ -251,6 +270,7 @@ function arnoldsTurn() {
 function playerTurn() {
   limitListenersOn();
   timerStart();
+
   $(".color-square1").on("click", function () {
     id = 1;
     squareClicked(id);
@@ -277,27 +297,32 @@ function playerTurn() {
     squareActive();
     patCheck();
   }
+
 }
 
 
 /* PLAYER PATTERN VERIFICATION */
 
 function patCheck() {
+
   if (sPat[sPat.length - (sPat.length - p1Pat.length) - 1] === p1Pat[p1Pat.length - 1] && p1Pat.length === sPat.length) {
     player1LastScore = p1Pat.length;
     timerStop();
     nextTurn();
     meterFill();
   } else if (sPat[sPat.length - (sPat.length - p1Pat.length) - 1] === p1Pat[p1Pat.length - 1]) {
-    if ( arnoldBool == true ) {
-      arnoldModeSwitch(); 
+
+    if (arnoldBool == true) {
+      arnoldModeSwitch();
     }
+
     currentRound++;
     hardMode1();
   } else {
     currentAudio.pause();
     gameOver();
   }
+
 }
 
 
@@ -311,14 +336,17 @@ function nextTurn() {
   levelBox();
   currentRound = 1;
   instructions(1);
+
   setTimeout(function () {
     message(1);
     message();
   }, 2000);
+
   setTimeout(function () {
     arnoldsTurn();
     waitAnimate();
   }, 3000 + (Math.ceil(Math.random() * 4) * 1000));
+
 }
 
 function playerTurnEnd() {
@@ -329,6 +357,7 @@ function playerTurnEnd() {
   $(".color-square4").off("click");
   $(".meter-btn").off("click");
 }
+
 
 /* GAME OVER */
 
@@ -369,10 +398,12 @@ function deathScreen() {
 
 function timerStart() {
   time = 0;
+
   counter = setInterval(function () {
     time++;
     $(".meter-timer").text(time);
   }, 1000);
+
 }
 
 function timerStop() {
@@ -382,7 +413,7 @@ function timerStop() {
 }
 
 
-/* METER */
+/* LIMIT METER */
 
 function meterReset() {
   meter = 0;
@@ -390,14 +421,17 @@ function meterReset() {
 }
 
 function meterFill() {
+
   if (turnLength < currentLevel) {
     meter = meter + ((currentLevel - turnLength) - 1);
   }
+
   let x = (meter * 5);
   let y = "calc(" + x + "% - 20px)";
   $(".meter-fill-bar").css("height", y);
 
   let displayCheck = replayBtn.attr("style");
+
   if (meter >= 20 && displayCheck == "display: block;") {
     clearCO.css("display", "block");
     replayCO.css("display", "block");
@@ -407,7 +441,9 @@ function meterFill() {
     limitOn();
     instructions(3);
   }
+
 }
+
 
 /* LIMIT BREAKS */
 
@@ -453,6 +489,7 @@ function replay() {
       listArnoldPat();
     }, i * 1000);
   }
+
 }
 
 
@@ -488,26 +525,31 @@ function limitListenersOn() {
 
 }
 
+
 /* CARRY-OVER LIMIT CHECK */
 
 function carryOverState() {
   let coDisplayCheck = replayCO.attr("style");
+
   if (coDisplayCheck == "display: block;") {
     limitOn();
     clearCO.css("display", "none");
     replayCO.css("display", "none");
   }
+
   meterReset();
 }
 
 
-/* LEVEL */
+/* LEVEL BOX*/
 
 function levelBox() {
   $(".level-num").addClass("is_level-num-change");
+
   setTimeout(function () {
     $(".level-num").text(currentLevel);
   }, 500);
+
   $(".level-num").on("animationend", function () {
     $(this).removeClass("is_level-num-change");
   });
@@ -517,12 +559,15 @@ function levelBox() {
 /* BOX SCORES */
 
 function highScore() {
+
   if (currentLevel === 1 && currentRound === 1) {
     player1LastScore = 0;
   }
+
   if (player1LastScore > player1HighScore) {
     player1HighScore = player1LastScore;
   }
+
 }
 
 function scorePush() {
@@ -533,18 +578,23 @@ function scorePush() {
 
 function pastScores() {
   scoreHist.push(player1LastScore);
+
   if (scoreHist.length > 1) {
     $("#player1ScoreBox2").text("Lvl " + scoreHist[scoreHist.length - 2]);
   }
+
   if (scoreHist.length > 2) {
     $("#player1ScoreBox3").text("Lvl " + scoreHist[scoreHist.length - 3]);
   }
+
   if (scoreHist.length > 3) {
     $("#player1ScoreBox4").text("Lvl " + scoreHist[scoreHist.length - 4]);
   }
+
   if (scoreHist.length > 4) {
     $("#player1ScoreBox5").text("Lvl " + scoreHist[scoreHist.length - 5]);
   }
+
 }
 
 
@@ -554,24 +604,30 @@ function message(number) {
 
   switch (number) {
     case 1:
+
       if (currentLevel === 10 || currentLevel === 15 || currentLevel === 20 || currentLevel === 25) {
         // do nothing 
       } else {
         $("#messages-pop").html("Wait for it <span class='anim_wait-pulse'>...</span>");
         $("#messages-pop").css("opacity", "1");
       }
+
       break;
+
     default:
   }
 
+  // During Party Mode color shift is disabled
   if (partyState == false) {
     // do nothing
   } else if (currentRound > 5) {
+
     if (Math.ceil(Math.random() * 8) === 1) {
       $("#messages-pop").text("So sorry bout this...");
       msgAnimate();
       colorChgRound();
     }
+
   }
 
   if (currentLevel === 10) {
@@ -643,22 +699,27 @@ function instructions(number) {
 clearBtn.mouseover(function () {
   instructions(4);
 });
+
 replayBtn.mouseover(function () {
   instructions(5);
 });
+
 clearCO.mouseover(function () {
   instructions(4);
 });
+
 replayCO.mouseover(function () {
   instructions(5);
 });
 
 function waitAnimate() {
   $("#messages-pop").addClass("anim_wait-pop");
+
   $("#messages-pop").on("animationend", function () {
     $(this).css("opacity", "0");
     $(this).removeClass("anim_wait-pop");
   });
+
 }
 
 function msgAnimate() {
@@ -678,7 +739,9 @@ function instructFade() {
 
 /* CHALLENGE CONDITIONS */
 
+// Chance to change color of color square above lvl 5 on click
 function hardMode1() {
+
   if (currentRound > 5 && Math.ceil(Math.random() * 2) === 1) {
     $(".anim_color-change").on("click", function () {
       $(this).css("backgroundColor", colorGen);
@@ -688,28 +751,34 @@ function hardMode1() {
       $(this).css("backgroundColor", colorGen);
     });
   }
+
 }
 
 
 /* COLOR CHANGING FUNCTIONS AND ASSIGNMENTS */
 
+// Random color generator
 function colorGen() {
   let r = Math.floor(Math.random() * 256);
   let g = Math.floor(Math.random() * 256);
   let b = Math.floor(Math.random() * 256);
   let colorValue = "rgb(" + r + ", " + g + ", " + b + ")";
+
   return colorValue;
 }
 
+// Random color assignment for Hard/Party mode
 function colorChgRound() {
   $(".anim_sq-glow").css("backgroundColor", colorGen);
 }
 
+// Screen element color change click listeners
 $("#scores").on("dblclick", function () {
   let elem = $(this);
   colorGrad(elem);
 });
 
+// CANT GET THIS TO WORK! LISTENER WILL NOT GRAB PARENT ELEMENT OF TARGET
 // $("#options").on("dblclick", function () {
 //   let elem = $(this);
 //   colorGrad(elem);
@@ -730,6 +799,7 @@ $("#header").on("dblclick", function () {
   colorGrad(elem);
 });
 
+// Change color and getComputedStyle during shift
 function colorGrad(elem) {
   elem.css("backgroundColor", colorGen);
   var colorShift = setInterval(function () {
@@ -774,7 +844,9 @@ let partyCounter = 0;
 let partyState = true;
 let interval = 1000;
 
+// Gradually increases rate of change of color squares 
 function partyTimerInterval() {
+
   if (interval <= 2) {
     interval = 1;
     document.getElementById("partyBtn").innerText = "Max Speed!";
@@ -785,16 +857,20 @@ function partyTimerInterval() {
   } else {
     interval = 1100 - (currentLevel * 100);
   }
+
 }
 
 function partyGameover() {
   interval = 1000;
+
   if (partyState == false) {
     partyToggle();
   }
+
 }
 
 function partyToggle() {
+
   if (partyState == true) {
     partyStarted();
     partyState = false;
@@ -804,23 +880,28 @@ function partyToggle() {
     partyState = true;
     document.getElementById("partyBtn").innerText = "Party Mode";
   }
+
 }
 
 function partyTempoUp() {
+
   if (partyState == false) {
     partysOver();
     partyTimerInterval();
     partyStarted();
   }
+
 }
 
 function partyShift() {
   colorSqArray[partyCounter].css("backgroundColor", colorGen);
+
   if (partyCounter === 3) {
     partyCounter = 0;
   } else {
     partyCounter++;
   }
+
 }
 
 function partyStarted() {
@@ -832,3 +913,4 @@ function partysOver() {
 }
 
 
+// || END of document 
