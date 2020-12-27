adb71982925d55cb495ab25faceca17312bf063b
AshleyThomasDevelopment <ashleythomasdevelopment@gmail.com>
2020-12-27 02:03:18 -0500

    added new arnold banks, changed folder structure, github cached folders not working

--- sounds.js
+++ sounds.js
@@ -52,124 +52,124 @@ $("#forceArnold").on("click", function () {
 });
 
 var arnoldSounds = [
-    "sounds/arnold1/how-are-you.mp3", 
-    "sounds/arnold1/howdy.mp3", 
-    "sounds/arnold1/idiot.mp3", 
-    "sounds/arnold1/sorry-2.mp3", 
-    "sounds/arnold1/hasta.mp3"
+    "sounds/arnoldBank1/how-are-you.mp3", 
+    "sounds/arnoldBank1/howdy.mp3", 
+    "sounds/arnoldBank1/idiot.mp3", 
+    "sounds/arnoldBank1/sorry-2.mp3", 
+    "sounds/arnoldBank1/hasta.mp3"
 ] 
  
 var arnoldSounds2 = [ 
-    "sounds/arnold2/do-it-now.mp3", 
-    "sounds/arnold2/answer-the-question.mp3", 
-    "sounds/arnold2/answered-immediately.mp3", 
-    "sounds/arnold2/i-want-to-ask-you-a-bunch-of-q.mp3",
-    "sounds/arnold2/who-is-your-daddy.mp3"
+    "sounds/arnoldBank2/do-it-now.mp3", 
+    "sounds/arnoldBank2/answer-the-question.mp3", 
+    "sounds/arnoldBank2/answered-immediately.mp3", 
+    "sounds/arnoldBank2/i-want-to-ask-you-a-bunch-of-q.mp3",
+    "sounds/arnoldBank2/who-is-your-daddy.mp3"
 ]
  
 var arnoldSounds3 = [ 
-    "sounds/arnold3/cookies.mp3", 
-    "sounds/arnold3/pasta.mp3", 
-    "sounds/arnold3/dont-u-understand.mp3", 
-    "sounds/arnold3/im-hungry.mp3", 
-    "sounds/arnold3/who-told-you-eat-my-cookies.mp3"
+    "sounds/arnoldBank3/cookies.mp3", 
+    "sounds/arnoldBank3/pasta.mp3", 
+    "sounds/arnoldBank3/dont-u-understand.mp3", 
+    "sounds/arnoldBank3/im-hungry.mp3", 
+    "sounds/arnoldBank3/who-told-you-eat-my-cookies.mp3"
 ]
 
 var arnoldSounds4 = [
-    "sounds/arnold4/hello.mp3", 
-    "sounds/arnold4/i-hear-you.mp3", 
-    "sounds/arnold4/its-me.mp3", 
-    "sounds/arnold4/who-are-you.mp3", 
-    "sounds/arnold4/i-dont-know-you-i-dont-answer.mp3"
+    "sounds/arnoldBank4/hello.mp3", 
+    "sounds/arnoldBank4/i-hear-you.mp3", 
+    "sounds/arnoldBank4/its-me.mp3", 
+    "sounds/arnoldBank4/who-are-you.mp3", 
+    "sounds/arnoldBank4/i-dont-know-you-i-dont-answer.mp3"
 ]
 
 var arnoldSounds5 = [
-    "sounds/arnold5/boris.mp3",
-    "sounds/arnold5/hauser.mp3",
-    "sounds/arnold5/quaid.mp3",
-    "sounds/arnold5/who-is-it-this-time.mp3",
-    "sounds/arnold5/get-ready-for-the-big-surprise.mp3"
+    "sounds/arnoldBank5/boris.mp3",
+    "sounds/arnoldBank5/hauser.mp3",
+    "sounds/arnoldBank5/quaid.mp3",
+    "sounds/arnoldBank5/who-is-it-this-time.mp3",
+    "sounds/arnoldBank5/get-ready-for-the-big-surprise.mp3"
 ]
 
 var arnoldSounds6 = [
-    "sounds/arnold6/get-your-mother-please.mp3",
-    "sounds/arnold6/just-do-it.mp3",
-    "sounds/arnold6/plz-get-ur-mother.mp3",
-    "sounds/arnold6/stop-whining.mp3",
-    "sounds/arnold6/you-think-im-funny.mp3"
+    "sounds/arnoldBank6/get-your-mother-please.mp3",
+    "sounds/arnoldBank6/just-do-it.mp3",
+    "sounds/arnoldBank6/plz-get-ur-mother.mp3",
+    "sounds/arnoldBank6/stop-whining.mp3",
+    "sounds/arnoldBank6/you-think-im-funny.mp3"
 ]
 
 var arnoldSounds7 = [
-    "sounds/arnold7/go-on.mp3",
-    "sounds/arnold7/i-dont-know.mp3",
-    "sounds/arnold7/whats-going-on1.mp3",
-    "sounds/arnold7/why-not.mp3",
-    "sounds/arnold7/going-on-right-now.mp3"
+    "sounds/arnoldBank7/go-on.mp3",
+    "sounds/arnoldBank7/i-dont-know.mp3",
+    "sounds/arnoldBank7/whats-going-on1.mp3",
+    "sounds/arnoldBank7/why-not.mp3",
+    "sounds/arnoldBank7/going-on-right-now.mp3"
 ]
 
 var arnoldSounds8 = [
-    "sounds/arnold8/first-i-would-like-to-get-to-know-you.mp3",
-    "sounds/arnold8/im-a-construction-worker.mp3",
-    "sounds/arnold8/im-a-police-officer.mp3",
-    "sounds/arnold8/im-a-terminator.mp3",
-    "sounds/arnold8/just-doing-my-job.mp3"
+    "sounds/arnoldBank8/first-i-would-like-to-get-to-know-you.mp3",
+    "sounds/arnoldBank8/im-a-construction-worker.mp3",
+    "sounds/arnoldBank8/im-a-police-officer.mp3",
+    "sounds/arnoldBank8/im-a-terminator.mp3",
+    "sounds/arnoldBank8/just-doing-my-job.mp3"
 ]
 
 var arnoldSounds9 = [
-    "sounds/arnold9/aliens.mp3",
-    "sounds/arnold9/disgusting (2).mp3",
-    "sounds/arnold9/dont-be-ridiculous.mp3",
-    "sounds/arnold9/no-it-is-not-true.mp3",
-    "sounds/arnold9/i-was-wrong-i-am-sorry.mp3"
+    "sounds/arnoldBank9/aliens.mp3",
+    "sounds/arnoldBank9/disgusting (2).mp3",
+    "sounds/arnoldBank9/dont-be-ridiculous.mp3",
+    "sounds/arnoldBank9/no-it-is-not-true.mp3",
+    "sounds/arnoldBank9/i-was-wrong-i-am-sorry.mp3"
 ]
 
 var arnoldSounds10 = [
-    "sounds/arnold10/no-no-no-no.mp3",
-    "sounds/arnold10/no.mp3",
-    "sounds/arnold10/oh-no.mp3",
-    "sounds/arnold10/stop-it.mp3",
-    "sounds/arnold10/i-rather-dont-talk-about-it.mp3"
+    "sounds/arnoldBank10/no-no-no-no.mp3",
+    "sounds/arnoldBank10/no.mp3",
+    "sounds/arnoldBank10/oh-no.mp3",
+    "sounds/arnoldBank10/stop-it.mp3",
+    "sounds/arnoldBank10/i-rather-dont-talk-about-it.mp3"
 ]
 
 var arnoldSounds11 = [
-    "sounds/arnold11/do-you-hear-what-i-said.mp3",
-    "sounds/arnold11/nice-to-meet-you.mp3",
-    "sounds/arnold11/you-lack-discipline.mp3",
-    "sounds/arnold11/you.mp3",
-    "sounds/arnold11/you-are-mine-now.mp3",
+    "sounds/arnoldBank11/do-you-hear-what-i-said.mp3",
+    "sounds/arnoldBank11/nice-to-meet-you.mp3",
+    "sounds/arnoldBank11/you-lack-discipline.mp3",
+    "sounds/arnoldBank11/you.mp3",
+    "sounds/arnoldBank11/you-are-mine-now.mp3",
 ]
 
 var arnoldSounds12 = [
-    "sounds/arnold12/da-na-na-na.mp3",
-    "sounds/arnold12/hello-cutiepie.mp3",
-    "sounds/arnold12/meet-me-halfway.mp3",
-    "sounds/arnold12/no-deal.mp3",
-    "sounds/arnold12/take-out-the-papers.mp3"
+    "sounds/arnoldBank12/da-na-na-na.mp3",
+    "sounds/arnoldBank12/hello-cutiepie.mp3",
+    "sounds/arnoldBank12/meet-me-halfway.mp3",
+    "sounds/arnoldBank12/no-deal.mp3",
+    "sounds/arnoldBank12/take-out-the-papers.mp3"
 ]
 
 var arnoldSounds13 = [
-    "sounds/arnold13/just-do-what-i-tell-you.mp3",
-    "sounds/arnold13/no-im-not-interested.mp3",
-    "sounds/arnold13/whats-2-week-package.mp3",
-    "sounds/arnold13/why-would-i-lie-to-you.mp3",
-    "sounds/arnold13/part-of-the-delightful-vacation.mp3",
+    "sounds/arnoldBank13/just-do-what-i-tell-you.mp3",
+    "sounds/arnoldBank13/no-im-not-interested.mp3",
+    "sounds/arnoldBank13/whats-2-week-package.mp3",
+    "sounds/arnoldBank13/why-would-i-lie-to-you.mp3",
+    "sounds/arnoldBank13/part-of-the-delightful-vacation.mp3",
 ]
 
 
 var arnoldSounds14 = [
-    "sounds/arnold14/dont-worry.mp3",
-    "sounds/arnold14/immediately.mp3",
-    "sounds/arnold14/quaid.mp3",
-    "sounds/arnold14/yeah.mp3",
-    "sounds/arnold14/tan.mp3",
+    "sounds/arnoldBank14/dont-worry.mp3",
+    "sounds/arnoldBank14/immediately.mp3",
+    "sounds/arnoldBank14/quaid.mp3",
+    "sounds/arnoldBank14/yeah.mp3",
+    "sounds/arnoldBank14/tan.mp3",
 ]
 
 var arnoldSounds15 = [
-    "sounds/arnold15/and-no-extra-charge.mp3",
-    "sounds/arnold15/ive-only-one-more-q.mp3",
-    "sounds/arnold15/wait-hold-on.mp3",
-    "sounds/arnold15/what-wrong-with-that.mp3",
-    "sounds/arnold15/what-the-hell-did-you-do.mp3"
+    "sounds/arnoldBank15/and-no-extra-charge.mp3",
+    "sounds/arnoldBank15/ive-only-one-more-q.mp3",
+    "sounds/arnoldBank15/wait-hold-on.mp3",
+    "sounds/arnoldBank15/what-wrong-with-that.mp3",
+    "sounds/arnoldBank15/what-the-hell-did-you-do.mp3"
 ]
 
 arnoldBank = [arnoldSounds, arnoldSounds2, arnoldSounds3, arnoldSounds4, arnoldSounds5, arnoldSounds6, arnoldSounds7, arnoldSounds8, arnoldSounds9, arnoldSounds10, arnoldSounds11, arnoldSounds12, arnoldSounds13, arnoldSounds14, arnoldSounds15 ];
